90919762b273681b7a0f789a4598fa2a85ab88a6
Arteen <arteen.galstyan@gmail.com>
2021-10-19 20:36:42 -0700

    Add delete functionality

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import Modal from './comps/Modal';
 
 function App() {
   const [selectedImg, setSelectedImg] = useState(null);
+  const [selectedImgId, setSelectedImgId] = useState(null);
 
   return (
     <div className="App">
       <Title/>
       <UploadForm />
-      <ImageGrid setSelectedImg={setSelectedImg} />
+      <ImageGrid setSelectedImg={setSelectedImg} setSelectedImgId={setSelectedImgId} />
       { selectedImg && (
-        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} />
+        <Modal selectedImg={selectedImg} setSelectedImg={setSelectedImg} selectedImgId={selectedImgId} setSelectedImgId={setSelectedImgId} />
       )}
     </div>
   );
